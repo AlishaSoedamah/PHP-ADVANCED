44c7dcffd3c7fee3a20dbcaad72da60bc5f44fa5
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2021-01-26 12:51:59 +0100

    does this work

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $Piet = array(
+        "voornaam" => "Piet",
+        "achternaam" => "Pat",
+        "leeftijd" => "17",
+        "klas" => "9A"
+    );
+    $Muhammad = array(
+        "voornaam" => "Muhammad",
+        "achternaam" => "Ali",
+        "leeftijd" => "17",
+        "klas" => "9G"
+    );
+    $Els = array(
+        "voornaam" => "Els",
+        "achternaam" => "Verheids",
+        "leeftijd" => "16",
+        "klas" => "0C"
+    );
+    echo $Els;
+    ?>
+</body>
+</html>
