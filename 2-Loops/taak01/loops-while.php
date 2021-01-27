891c264324e59259fe350e6e7d8cb2db8283d154
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2021-01-27 12:15:10 +0100

    foreach loops

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -15,6 +15,10 @@
         echo "Ik ben: $leeftijd jaar dus ik mag nog niet stemmen <br>";
         $leeftijd++;
         } 
+        ?>
+    </li>
+        <li>
+        <?php
         echo "Ik ben 18 jaar oud dus ik heb stemrecht!"
         ?> 
         </li>
---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mijn gegevens</title>
+</head>
+<body>
+    
+    <?php echo "Voornaam is:".$_POST["voornaam"]; ?><br>
+      Achternaam is:<?php echo $_POST["achternaam"]; ?><br>
+      Klas is:<?php echo $_POST["klas"]; ?><br>
+      Leeftijd is:<?php echo $_POST["leeftijd"]; ?><br>
+      Adres is:<?php echo $_POST["adres"]; ?><br>
+      Plaatsnaam is:<?php echo $_POST["plaatsnaam"];?><br>
+      Favoriete muziekband is:<?php echo $_POST["a"];?><br>
+</body>
+</html>
