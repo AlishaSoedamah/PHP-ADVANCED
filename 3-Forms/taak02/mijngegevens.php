2f4dba718f2f528b07a7d2812b1d09ead1246d14
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2021-01-28 14:54:25 +0100

    SICK & TIRED

---
+++ 3-Forms/taak02/mijngegevens.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mijn gegevens</title>
+</head>
+<body>
+    
+    <?php echo "Voornaam is:".$_POST["voornaam"]; ?><br>
+      Geslacht:<?php echo $_POST["geslacht"];?><br>
+      Achternaam:<?php echo $_POST["achternaam"]; ?><br>
+      Klas:<?php echo $_POST["klas"]; ?><br>
+      Leeftijd:<?php echo $_POST["leeftijd"]; ?><br>
+      Adres:<?php echo $_POST["adres"]; ?><br>
+      Plaatsnaam:<?php echo $_POST["plaatsnaam"];?><br>
+      Favoriete muziekband:<?php echo $_POST["a"];?><br>
+      <?php echo $_POST["geslacht"];?><BR>
+      echo '<body style="background-color:orange">';<
+</body>
+</html>
