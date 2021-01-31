a4ab98c2ac375a478a24d9e52b038ba9360c48da
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2021-01-31 15:53:41 +0100

    form

--- 3-Forms/taak02/mijngegevens.php
+++ 3-Forms/taak02/mijngegevens.php
@@ -1,9 +1,18 @@
+<?php
+$colour = $_POST["colour"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mijn gegevens</title>
+        <style>
+            body{
+                background-color: <?php echo $colour;?>
+            }
+        </style>
 </head>
 <body>
     
@@ -15,7 +24,9 @@
       Adres:<?php echo $_POST["adres"]; ?><br>
       Plaatsnaam:<?php echo $_POST["plaatsnaam"];?><br>
       Favoriete muziekband:<?php echo $_POST["a"];?><br>
-      <?php echo $_POST["geslacht"];?><BR>
-      echo '<body style="background-color:orange">';<
+      <?php echo $_POST["geslacht"];?><br>
+
+      <!-- I AM TRYING TO MAKE THE COLOUR APEAR ON THIS SCREEN WITH THE GIVEN USER INPUT.-->
+      <?php echo $colour; ?>
 </body>
 </html>
---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "Mijn favoriete artiest heet ". $_GET["artiest"]. "Echt mijn lied is ".$_GET["titel"] "Jaar van uitkomst".$_GET["jaarvanuitkomst"]. "en muzieksoort is". $_GET["muzieksoort"];
+    ?>
+</body>
+</html>
