1ae95ae871a790c41bbd60ce894185ad571fc033
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2021-02-10 15:07:29 +0100

    redone ul

--- 2-Loops/taak02/loops-for.php
+++ 2-Loops/taak02/loops-for.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    
+
     for ($x = 0; $x < 18; $x++) {
-        echo "Ik ben $x jaar, dus ik mag nog niet stemmen <br>";
-      }
-      echo "Ik ben 18 jaar oud dus ik heb stemrecht!";
+    }
+
     ?>
+    <ul>
+        <?php for ($x = 0; $x < 18; $x++) { ?>
+            <li>
+                <?php echo "Ik ben $x jaar, dus ik mag nog niet stemmen <br>"; ?>
+            </li>
+        <?php } ?>
+    </ul>
+    <ul>
+        <li><?php echo "Ik ben 18 jaar, dus ik heb stemrecht!!!!!" ?></li>
+    </ul>
 </body>
+
 </html>
--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -1,22 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <form action="advanced-1.php">
-    <label for="klas">Kies een kleur:</label>
-    <select id="kleuren" name="">
-    <?php
-    $kleuren = array("red", "blue", "green", "black", "brown")
 
-    foreach($kleuren as $value){
-        echo "<option value='strtolower($value)'>$value</option>";
-    }
-    ?>
-    <input type="submit">
+<body>
+    <form action="advanced-2.php" method="post">
+        <label for="klas">Kies een kleur:</label>
+        <select id="kleuren" name="kleuren">
+            <!--kleuren tabel-->
+            <?php
+            $kleuren = array(
+                "red",
+                "blue",
+                "green",
+                "black",
+                "brown"
+            );
+            foreach ($kleuren as $value) {
+                echo "<option value='strtolower($value)'>$value</option>";
+            ?>
+                <option value="<?php echo $value ?>"><?php echo $value ?></option>
+            <?php } ?>
+            <!--tabel-border dikte-->
+            <!--padding in pixels-->
+        </select>
+        <input type="submit" name="submit" value="verstuur">
     </form>
 </body>
+
 </html>
